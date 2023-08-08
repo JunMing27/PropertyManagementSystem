6f3601c4f07bdce6157d430eb3b5f1d0de8f06d4
JunMing27 <98100812+JunMing27@users.noreply.github.com>
2023-08-08 10:46:41 +0800

    cant debug, later compile d only see how

--- DataStructure2/property/property.h
+++ DataStructure2/property/property.h
@@ -23,19 +23,10 @@ struct Property {
     Property* next;
 };
 
-extern Property* pHead; // Declare pHead as extern
-extern Property* pTail; // Declare pTail as extern
 
 void addNewProperty(string ads_id, string prop_name, string completion_year, string monthly_rent, string location, string propertyType, string rooms, string parking, string bathroom, string size, string furnished, string facilities, string additional_facilities, string region);
 void initializeProperty();
 void inputTenantPropertyMenu();
 void displayTenantTenancyPropInfo(string PropId);
-bool compareAttributeDesc(const string& attribute1, const string& attribute2);
-void mergeSortForBinarySearch(Property* pHead, string searchCriteria);
-void mergeForBinarySearch(Property*& pHead, Property* left, Property* mid, Property* right, string searchCriteria);
-Property* getMidNode(Property* head, int size);
-Property* getTailNode(Property* head);
-Property* binarySearchOnSortedList(Property* head, string searchCriteria, string searchInput);
-Property* getMidNode(Property* low, Property* high);
 void linearOrBinary(string searchCriteria, string searchMethod, string searchInput);
 void displaySearchResult(Property* result);
